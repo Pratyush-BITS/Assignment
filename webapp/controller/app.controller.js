e11506aa508f03f20c9a6b0b5095c40dd0e92108
Pratyush-BITS <2021sp93070@wilp.bits-pilani.ac.in>
2022-01-17 20:32:57 +0530

    set up datasource
    update manifest with datasource and add debug instruction to check the same

--- webapp/controller/app.controller.js
+++ webapp/controller/app.controller.js
@@ -1,15 +1,25 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
+	function (Controller,MessageToast) {
 		"use strict";
 
 		return Controller.extend("com.learn.assignment.controller.app", {
 			onInit: function () {
-
+				MessageToast.show("Haai");
+			},
+			
+			/*
+			Add a debugger on Message toast after loading the page,
+			goto console and write 
+			this.getView().getModel('storehouse').getJSON();
+			*/
+			onPress: function(){
+				MessageToast.show("For Debugger")
 			}
 		});
 	});
