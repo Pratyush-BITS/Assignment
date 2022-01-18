5a8e1f8f77d3e847eaa7b9e99d369b738bad35f2
Pratyush-BITS <2021sp93070@wilp.bits-pilani.ac.in>
2022-01-18 23:48:10 +0530

    table oDatabinding
    made global variable to increase code reuse and done binding on storehouse -> stock to the table so that sap.m.table gets updated on each entry

--- webapp/controller/app.controller.js
+++ webapp/controller/app.controller.js
@@ -1,30 +1,56 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller,MessageToast) {
+	function (Controller, MessageToast, JSONModel) {
 		"use strict";
+
+		var _fields = {}
 
 		return Controller.extend("com.learn.assignment.controller.app", {
 			onInit: function () {
 				MessageToast.show("Haai");
+				_fields.material = this.byId('material'),
+				_fields.segment = this.byId('segment'),
+				_fields.batch = this.byId('batch'),
+				_fields.quantity = this.byId('quantity')
 			},
 			onKeyPress: function(oEvent){
-				var _oQuantity= oEvent.getSource();
-				var text = _oQuantity.getValue();
+				var text = _fields.quantity.getValue();
 				text = text.replace(/[^\d]/g, '');
-				_oQuantity.setValue(text);
+				_fields.quantity.setValue(text);
 			},
 			onSubmit: function(oEvent){
+				var materialVal = _fields.material.getValue()
+				var segmentVal = _fields.segment.getValue();
+				var batchVal = _fields.batch.getValue();
+				var quantityVal = _fields.quantity.getValue();
+
+				if( materialVal &&  segmentVal && batchVal && quantityVal){
+						var storeHouseModel = this.getView().getModel('storehouse');
+						var data = {
+							'Material':materialVal,
+							'Segment':segmentVal,
+							'Batch':_fields.batchVal,
+							'Quantity':quantityVal
+						};
+						var oColListItem = this.getView().byId('colListItem');
+						oTable.bindItems("storehouse>/stock",oColListItem,null,null);
+						this.onClear();
+					}
+				else{
+					MessageToast.show("Please enter proper data in all the fields")
+				}
 			},
-			onClear:function(oEvent){
-				this.byId('material').setValue('');
-				this.byId('segment').setValue('');
-				this.byId('batch').setValue('');
-				this.byId('quantity').setValue('');
+			onClear:function(){
+				_fields.material.setValue('');
+				_fields.segment.setValue('');
+				_fields.batch.setValue('');
+				_fields.quantity.setValue('');
 			}
 		});
 	});
